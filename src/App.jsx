3fcf93ee7c1d308d44a37febd3cb0024923fd75e
Thais Bertoldo <thaisbertoldo11@gmail.com>
2022-08-09 22:16:24 -0300

    feat: aula 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import './App.css';
-import { Footer } from './components/Footer/Footer';
-import { Header } from './components/Header/Header';
 import { Post } from './components/Post/Post';
 
+import capaPost1 from './assets/capa.jpeg';
+import capaPost2 from './assets/vite.svg';
+import { Card } from './components/Card/Card';
+import { Layout } from './components/Layout/Layout';
+
 export function App() {
+  const onClick = (event) => {
+    console.log(event);
+    console.log('Clicou');
+  };
+
   return (
-    <div className='container'>
-      <Header />
+    <Layout>
+      <Card>
+        <h1>Meu Card</h1>
+        <p>Teste</p>
+        <footer>Rodapé</footer>
+      </Card>
+      <br />
+      <Card>
+        <img src={capaPost1} alt='aas' />
+        <p>Teste</p>
+      </Card>
 
-      <Post />
+      <Post
+        titulo='Minha matéria'
+        descricao='ljansjansjans janjnkjansjansjansjk ja sja '
+        capa={capaPost1}
+        data={new Date().toLocaleDateString()}
+        handleClick={onClick}
+      />
 
-      <Footer />
-    </div>
+      <Post
+        titulo='Nossa matéria'
+        descricao='saslokomiwq w asuhhu auhsuashua iqjwioqjwiqw'
+        capa={capaPost2}
+        handleClick={() => console.log('arrow')}
+      />
+
+      <Post descricao='aksjaiksjka saksjkajsaks' handleClick={onClick} />
+    </Layout>
   );
 }
