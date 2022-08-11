be155c7e43082395924563f38a472c6af1df5cb2
Thais Bertoldo <thaisbertoldo11@gmail.com>
2022-08-10 23:00:11 -0300

    feat: aula-03

---
+++ src/components/Pessoa/Pessoa.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+export const Pessoa = ({ pessoa }) => {
+  return (
+    <div>
+      <p>Nome: </p>
+      <strong>{pessoa.nome}</strong>
+      <p>Idade: </p>
+      <strong>{pessoa.idade}</strong>
+      <p>Apelido: </p>
+      <strong>{pessoa.apelido || 'Sem apelido'}</strong>
+    </div>
+  );
+};
+
+Pessoa.propTypes = {
+  pessoa: PropTypes.shape({
+    nome: PropTypes.string.isRequired,
+    idade: PropTypes.number.isRequired,
+    apelido: PropTypes.string,
+  }),
+};
